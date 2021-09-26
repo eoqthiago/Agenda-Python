9f4f0da6600b3e1cfbb67992a8c68c587d9fd5bb
eoqthiago <contatopixthiago@gmail.com>
2021-09-26 02:47:21 -0300

    primeiro commit

---
+++ agenda/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path
+from core import views
+from django.views.generic import RedirectView
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('agenda/', views.lista_eventos),
+    path("", RedirectView.as_view(url='/agenda/'))
+
+
+]
+
+
